084e08cbfc49d64e52343e8f5155aa03b2efb911
Chris Dooley <chris.dooley@amt-sybex.com>
2017-11-10 13:33:37 +0000

    Add Get Todos Route

--- server/server.js
+++ server/server.js
@@ -27,6 +27,14 @@ app.post('/todos',(req,res)=>{
 
 });
 
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  })
+});
+
 //Set up web server
 app.listen(3000,()=>{
   console.log('Server Started:Listening on port 3000');
