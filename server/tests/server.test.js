27ebc600245bd3f5db417bff5dda3b16b1f03f7e
Chris Dooley <chris.dooley@amt-sybex.com>
2017-11-15 11:58:17 +0000

    add tests for /TODOS

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,14 +3,21 @@ const request=require('supertest');
 const {app}=require('./../server');
 const {Todo}=require('./../models/todo');
 
+const todos=[{
+  text:'ONE test todo'
+},{
+  text:'TWO test todo'
+}];
 
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=>done());
 });
 
 describe('POST /todos',()=>{
   it('should create a new todo',(done)=>{
-      var text='test todo text';
+      var text='THREE test todo';
 
       request(app)
       .post('/todos')
@@ -24,8 +31,8 @@ describe('POST /todos',()=>{
           return done(err);
         }
         Todo.find().then((todos)=>{
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
+          expect(todos.length).toBe(3);
+          expect(todos[0].text).toBe('ONE test todo');
           done();
         }).catch((e)=>done(e));
       });
@@ -41,15 +48,22 @@ describe('POST /todos',()=>{
         return done(err);
       }
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e)=>done(e));
     });
-
   });
 
-
-
-
-
 });
+
+  describe('GET/todos',()=>{
+    it('should get all Todos',(done)=>{
+      request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{
+          expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
+  });
