b7aa5d4fd5e9838801788f1cc9e29d3831631f1d
Chris Dooley <chris.dooley@amt-sybex.com>
2017-11-16 14:47:14 +0000

    Add Get /Todos/id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var bodyParser=require('body-parser');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
+const {ObjectID}=require('mongodb');
 
 //Use Express
 var app=express();
@@ -34,6 +35,21 @@ app.get('/todos',(req,res)=>{
     res.status(400).send(e);
   })
 });
+
+// GET /todos/1234324
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id
+  if(!ObjectID.isValid(id)){
+    return res.status(400).send();
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+      return res.status(200).send({todo});
+  }).catch((e)=>res.status(400).send());
+});
+
 
 //Set up web server
 app.listen(3000,()=>{
