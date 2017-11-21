fe46a630f22b0f7bcf9dab25ce8d3a74af06ab63
Chris Dooley <chris.dooley@amt-sybex.com>
2017-11-21 16:24:09 +0000

    added PATCH

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express=require('express');
-var bodyParser=require('body-parser');
+const _=require('lodash');
+const express=require('express');
+const bodyParser=require('body-parser');
 
 //Deconstruction allows you to create a constant with is an object directly from a file
 var {mongoose}=require('./db/mongoose');
@@ -65,6 +66,35 @@ app.delete('/todos/:id',(req,res)=>{
       return res.status(200).send({todo});
   }).catch((e)=>res.status(400).send());
 });
+
+app.patch('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  //subset of things user passed
+  var body=_.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(400).send();
+  }
+  //update completed by properties
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt=new Date().getTime();
+  }else{
+    body.completed=false;
+    body.completedAt=null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch((e)=>{
+    response.status(400).send();
+  })
+});
+
 
 //Set up web server
 app.listen(port,()=>{
