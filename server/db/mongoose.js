3e10e4bd997a13e50dbaff8427bbc5262e1f6ea1
Chris Dooley <chris.dooley@amt-sybex.com>
2017-11-22 11:47:50 +0000

    For heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose=require('mongoose');
 
 //Configure Mongoose
 mongoose.Promise=global.Promise;
-//mongoose.connect('mongodb://chris14dooley:Dangermouse14mLab@ds113136.mlab.com:13136/nodedbcourse'||'mongodb://localhost:27017/TodoApp');
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://chris14dooley:Dangermouse14mLab@ds113136.mlab.com:13136/nodedbcourse'||'mongodb://localhost:27017/TodoApp');
+//mongoose.connect(process.env.MONGODB_URI);
 
 module.exports={mongoose};
