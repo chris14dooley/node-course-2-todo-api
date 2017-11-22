d7881c6f31dbb47bac32a64d856696d71208153a
Chris Dooley <chris.dooley@amt-sybex.com>
2017-11-22 11:44:02 +0000

    Setup envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose=require('mongoose');
 //Configure Mongoose
 mongoose.Promise=global.Promise;
 //mongoose.connect('mongodb://chris14dooley:Dangermouse14mLab@ds113136.mlab.com:13136/nodedbcourse'||'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports={mongoose};
