36a493fb25fdf1482caf387c78b74c990f4004c2
Chris Dooley <chris.dooley@amt-sybex.com>
2017-11-30 12:03:56 +0000

    add post users login route

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,28 @@ UserSchema.methods.generateAuthToken=function () {
 };
 
 //Object level methods
+
+UserSchema.statics.findByCredentials=function(email,password){
+  var User=this;
+
+  return User.findOne({email}).then((user)=>{
+    if(!user){
+        return Promise.reject('invalid user');
+    }
+
+    return new Promise((resolve,reject)=>{
+
+      bcrypt.compare(password,user.password,(err,res)=>{
+        if(res){
+          resolve(user);
+        }else{
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.statics.findByToken=function (token) {
   var User=this;
   var decoded;
--- server/server.js
+++ server/server.js
@@ -18,8 +18,22 @@ const port=process.env.PORT;
 //configure middleware to use BodyParser
 app.use(bodyParser.json());
 
+//Authenticate function uses middleware to authenticate token
 app.get('/users/me',authenticate,(req,res)=>{
   res.send(req.user);
+});
+
+app.post('/users/login',(req,res)=>{
+    var body=_.pick(req.body,['email','password']);
+
+    //verify that user exists
+    User.findByCredentials(body.email,body.password).then((user)=>{
+      return user.generateAuthToken().then((token)=>{
+        res.header('x-auth',token).send(user);
+      });
+    }).catch((err)=>{
+      res.status(400).send();
+    });
 });
 
 //Set up post Route
