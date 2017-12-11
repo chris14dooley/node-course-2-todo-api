966b408d6f01d6423b5b2619d6fc60295fecde43
Chris Dooley <chris.dooley@amt-sybex.com>
2017-12-11 12:07:41 +0000

    update config to use JSON

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -24,7 +24,7 @@ const users=[{
   password:'userOnePass',
   tokens:[{
     access:'auth',
-    token: jwt.sign({_id:userOneID,access:'auth'},'abc123').toString()
+    token: jwt.sign({_id:userOneID,access:'auth'},process.env.JWT_SECRET).toString()
   }]
 },{
   _id:userTwoID,
@@ -32,7 +32,7 @@ const users=[{
   password:'userTwoPass',
   tokens:[{
     access:'auth',
-    token: jwt.sign({_id:userTwoID,access:'auth'},'abc123').toString()
+    token: jwt.sign({_id:userTwoID,access:'auth'},process.env.JWT_SECRET).toString()
   }]
 }];
 
